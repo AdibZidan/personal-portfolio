ed4bf8bd4a11dfddbb220434e095999093f3b90d
AdibZidan <94amsz@gmail.com>
2019-05-14 18:18:01 +0200

    modify app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title: string = 'I4 Angular Projects';
+  title: string = "My Angular Projects";
 }
