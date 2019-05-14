f58c69d32156f08b8177437be8c4d5d55fe4f8d3
AdibZidan <94amsz@gmail.com>
2019-05-14 18:20:02 +0200

    modify app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TasksComponent } from "./task-manager/tasks/tasks.component";
+import { ChessComponent } from "./chess/chess/chess.component";
+import { TaskDetailComponent } from "./task-manager/task-detail/task-detail.component";
+import { MessagesComponent } from "./task-manager/messages/messages.component";
+import { PortfolioComponent } from './portfolio/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    ChessComponent,
+    TaskDetailComponent,
+    MessagesComponent,
+    PortfolioComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
