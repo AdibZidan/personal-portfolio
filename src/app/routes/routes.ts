bac84d3d51797b670451ecf8bc931914dc86eb43
AdibZidan <94amsz@gmail.com>
2019-05-15 10:25:52 +0200

    use relative imports

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,7 +3,7 @@ import { Routes } from "@angular/router";
 import { TasksComponent } from "../task-manager/tasks/tasks.component";
 import { ChessComponent } from "../chess/chess/chess.component";
 import { PortfolioComponent } from "../portfolio/portfolio/portfolio.component";
-import { TodosComponent } from "../todo/todos/todos.component";
+// import { TodosComponent } from "../todo/todos/todos.component";
 
 export const ROUTES: Routes = [
   { path: "", redirectTo: "portfolio", pathMatch: "full" },
