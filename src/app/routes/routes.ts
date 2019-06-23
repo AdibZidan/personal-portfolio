b184ca36aa9b531f2bb8936197becaf4e76bf31a
AdibZidan <94amsz@gmail.com>
2019-06-23 19:33:03 +0200

    Imported the EditComponent and added it to the ROUTES array

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { PortfolioComponent } from '../portfolio/portfolio/portfolio.component';
 import { MainComponent } from '../todo/main/main.component';
+import { EditComponent } from '../todo/edit/edit.component';
 
 export const ROUTES: Routes = [
   { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
   { path: 'portfolio', component: PortfolioComponent },
-  { path: 'todo', component: MainComponent }
+  { path: 'todo', component: MainComponent },
+  { path: 'edit-todo/:id', component: EditComponent }
 ];
