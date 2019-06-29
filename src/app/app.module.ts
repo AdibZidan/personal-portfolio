642837dfebee1a77db405e3431f76bc5fb80bc37
AdibZidan <94amsz@gmail.com>
2019-06-29 07:56:10 +0200

    Imported the PortfolioComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 
 import { HeaderComponent } from './header/header.component';
-import { PortfolioComponent } from './portfolio/portfolio/portfolio.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 import { FooterComponent } from './footer/footer.component';
 
 import { BrowserModule } from "@angular/platform-browser";
