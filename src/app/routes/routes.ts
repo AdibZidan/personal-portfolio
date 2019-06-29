ce285c3c61b76fb06f7258a74264c1db6211a8cb
AdibZidan <94amsz@gmail.com>
2019-06-29 07:56:52 +0200

    Imported the PortfolioComponent

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
-import { PortfolioComponent } from '../portfolio/portfolio/portfolio.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
 import { MainComponent } from '../todo/main/main.component';
 import { EditComponent } from '../todo/edit/edit.component';
 
