e8605e2a89f429ee0dbc1d9651fa772af9b427dc
AdibZidan <94amsz@gmail.com>
2019-07-05 21:01:30 +0200

    Implemented ngOnDestroy

--- src/app/todo/dialog/dialog.component.ts
+++ src/app/todo/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
 
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
@@ -6,13 +6,17 @@ import { FormComponent } from '../form/form.component';
 
 import { Todo } from 'src/app/classes/Todo';
 
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
 
-export class DialogComponent implements OnInit {
+export class DialogComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+
   @Input() todo: Todo;
 
   @Output() save: EventEmitter<Todo> = new EventEmitter<Todo>();
@@ -23,6 +27,11 @@ export class DialogComponent implements OnInit {
   ) { }
 
   ngOnInit() { }
+
+  ngOnDestroy(): void {
+    if (this.subscription !== undefined) { this.subscription.unsubscribe(); }
+    console.log('Dialog unsubscribed!');
+  }
 
   onClick() {
     this.matDialogRef = this.dialog.open(FormComponent, {
