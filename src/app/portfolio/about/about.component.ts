861cb9c4340202e7c9c30892ae3bae9166f5f1c2
AdibZidan <94amsz@gmail.com>
2019-07-11 22:59:21 +0200

    Work in progress - Created the languages array

--- src/app/portfolio/about/about.component.ts
+++ src/app/portfolio/about/about.component.ts
@@ -5,11 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
+
 export class AboutComponent implements OnInit {
+
+  languages: string[] = ['English', 'Arabic', 'Russian', '& German'];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
