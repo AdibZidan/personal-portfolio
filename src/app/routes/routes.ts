5b1dec6f572e4a1a2a813f1c5c6ffd1c04f8fa4e
AdibZidan <94amsz@gmail.com>
2019-07-11 22:56:34 +0200

    Imported the AboutComponent and created path

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { PortfolioComponent } from '../portfolio/portfolio.component';
+
+import { AboutComponent } from './../portfolio/about/about.component';
+
 import { MainComponent } from '../task-manager/main/main.component';
 
 export const ROUTES: Routes = [
   { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
   { path: 'portfolio', component: PortfolioComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'task-manager', component: MainComponent }
 ];
