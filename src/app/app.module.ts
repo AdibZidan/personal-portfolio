bc9a6b799fa466f9f1f29ecf291c394144b41282
Adib Zidan <94amsz@gmail.com>
2019-07-13 10:32:30 +0200

    Removed every related component to portfolio and imported PortfolioModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from './portfolio/header/header.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { AboutComponent } from './portfolio/about/about.component';
-import { FooterComponent } from './portfolio/footer/footer.component';
 
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule } from "@angular/common/http";
+import { PortfolioModule } from './portfolio/module/portfolio.module';
 import { TaskModule } from './task-manager/module/task.module';
 
 import { MAT_DIALOG_DATA } from '@angular/material';
@@ -16,15 +14,13 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    PortfolioComponent,
-    AboutComponent,
-    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    PortfolioModule,
     TaskModule
   ],
   providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }],
