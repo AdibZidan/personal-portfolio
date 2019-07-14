38f663c56d15d11011997bdf5f78e1b394b9e9a2
AdibZidan <94amsz@gmail.com>
2019-07-14 19:02:24 +0200

    Added the animations array and created the prepare method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
+import { easeIn } from './../assets/animations/animation';
+
+import { RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [easeIn]
 })
 
-export class AppComponent { }
+export class AppComponent {
+
+  prepare(outlet: RouterOutlet) {
+    const animation: RouterOutlet = outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+
+    return animation
+  }
+
+}
