84b8f4323d059bc25adad49c7af2243b8e369c00
Adib Zidan <94amsz@gmail.com>
2019-07-17 21:23:10 +0200

    Seperated TaskService spec into a mock folder

---
+++ src/app/task-manager/services/mock/mock-service.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { TaskService } from '../task.service';
+import { Task } from '../../interface/Task';
+import { tasksMock, taskMock } from './mock';
+
+export let taskService: TaskService, httpTestingController: HttpTestingController;
+
+export let mockTasks: Task[], mockTask: Task;
+
+beforeEach(() => {
+
+    mockTasks = tasksMock;
+
+    mockTask = taskMock;
+
+    TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        providers: [TaskService]
+    });
+
+    taskService = TestBed.get(TaskService);
+
+    httpTestingController = TestBed.get(HttpTestingController);
+
+});
+
+afterEach(() => { httpTestingController.verify(); });
