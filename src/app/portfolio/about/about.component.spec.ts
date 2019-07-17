9dac2b5aa344c35150c06c28d7c19fab43db49e5
AdibZidan <94amsz@gmail.com>
2019-07-17 15:13:23 +0200

    Added 2 tests; languages and technologyStacks

--- src/app/portfolio/about/about.component.spec.ts
+++ src/app/portfolio/about/about.component.spec.ts
@@ -2,13 +2,42 @@ import { AboutComponent } from "./about.component";
 
 describe('About Component', () => {
 
+  let aboutComponent: AboutComponent;
+
   it('Should exist', () => { expect(AboutComponent).toBeDefined(); });
+
+  beforeEach(() => { aboutComponent = new AboutComponent(); });
 
   it('Should be built with zero arguments', () => {
 
-    const aboutComponent = new AboutComponent();
-
     expect(aboutComponent instanceof AboutComponent).toBe(true);
+
+  });
+
+  it('Should have languages array', () => {
+
+    const expectedLanguages: string[] = [
+      'English,', 'Arabic,', 'Russian', '& German.'
+    ];
+
+    const actualLanguages = aboutComponent.languages;
+
+    expect(expectedLanguages).toEqual(actualLanguages);
+
+  });
+
+  it('Should have technologyStacks array', () => {
+
+    const expectedTechnologyStacks: string[] = [
+      'HTML5,', 'CSS3,', 'SCSS,',
+      'JavaScript ES6+,', 'TypeScript,', 'Angular 8,',
+      'Docker,', 'Git/GitHub,', 'Ubuntu,',
+      'Node.JS,', 'npm,', '& Visual Studio Code.'
+    ];
+
+    const actualTechnologyStacks = aboutComponent.technologyStacks;
+
+    expect(expectedTechnologyStacks).toEqual(actualTechnologyStacks);
 
   });
 
