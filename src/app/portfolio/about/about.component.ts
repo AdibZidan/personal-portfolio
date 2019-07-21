d357979700bcfbc1d460ab63f99ab0fd23a75104
AdibZidan <94amsz@gmail.com>
2019-07-21 08:02:19 +0200

    Imported 'HostListener', created 'scrollUp', 'isUserOnBottom' and set some types

--- src/app/portfolio/about/about.component.ts
+++ src/app/portfolio/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AboutComponent implements OnInit {
+
+  showArrow: boolean = false;
 
   languages: string[] = ['English,', 'Arabic,', 'Russian', '& German.'];
 
@@ -19,7 +21,24 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit(): void { }
 
-  onClick() { window.scroll({ top: 0, left: 0, behavior: 'smooth' }); }
+  onClick(): void {
+    this.showArrow = false;
+    this.scrollUp();
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void { this.isUserOnBottom(); }
+
+  private scrollUp(): void { window.scroll({ top: 0, left: 0, behavior: 'smooth' }); }
+
+  private isUserOnBottom(): void { this.onBottom(); }
+
+  private onBottom(): void {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      this.showArrow = true;
+    }
+  }
+
 }
