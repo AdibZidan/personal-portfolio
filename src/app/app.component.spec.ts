81f0afecbc30fa116c989b3a75153012b822bdb1
AdibZidan <94amsz@gmail.com>
2019-07-25 07:37:26 +0200

    Modified 'AppComponent's' spec file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,13 @@ import { AppComponent } from './app.component';
 
 describe('Application Component', () => {
 
-  it('Should exist', () => { expect(AppComponent).toBeDefined(); });
+  it('Should exist', () => expect(AppComponent).toBeDefined());
 
   it('Should be built with zero arguments', () => {
 
     const appComponent = new AppComponent();
 
-    expect(appComponent instanceof AppComponent).toBe(true);
+    expect(appComponent instanceof AppComponent).toBeTruthy();
 
   });
 
