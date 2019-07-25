9e64632f78d205838e3d0be394b243b97c460537
Adib Zidan <94amsz@gmail.com>
2019-07-25 10:58:51 +0200

    Created a language folder

---
+++ src/app/portfolio/about/language/technology-stack.ts
@@ -0,0 +1,12 @@
+export const frontEnd: string[] = [
+    'HTML5,', 'CSS3 / SCSS,',
+    'JavaScript ES6+ / TypeScript,',
+    'and Angular 8'
+];
+
+export const backEnd: string = 'Node.JS';
+
+export const other: string[] = [
+    'Docker,', 'Git/GitHub,', 'Ubuntu,',
+    'Windows 10,', 'NPM', '& Visual Studio Code.'
+];
