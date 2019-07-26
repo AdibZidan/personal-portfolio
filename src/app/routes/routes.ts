511c266d21ebca020955b4f2209851b9907945a0
Adib Zidan <94amsz@gmail.com>
2019-07-26 15:42:56 +0200

    Imported 'lazyLoadPortfolioComponent' into loadChildren property

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,24 +1,14 @@
 import { Routes } from '@angular/router';
 
+import { lazyLoadPortfolioComponent } from './lazy-load/lazyLoadComponent';
+
 import { AboutComponent } from './../portfolio/about/about.component';
 
 import { MainComponent } from './../task-manager/main/main.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'portfolio',
-    pathMatch: 'full'
-  },
-  {
-    path: 'portfolio',
-    loadChildren: () => import('../portfolio/route/portfolio.module').then(module => module.PortfolioModule),
-    data: { animation: 'portfolio' }
-  },
+  { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
+  { path: 'portfolio', loadChildren: lazyLoadPortfolioComponent, data: { animation: 'portfolio' } },
   { path: 'about-me', component: AboutComponent, data: { animation: 'about-me' } },
-  {
-    path: 'task-manager',
-    component: MainComponent,
-    data: { animation: 'task-manager' }
-  }
+  { path: 'task-manager', component: MainComponent, data: { animation: 'task-manager' } }
 ];
