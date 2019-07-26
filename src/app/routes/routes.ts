6b04afebcc4cb13d84fa9697767472e3b2742ffc
Adib Zidan <94amsz@gmail.com>
2019-07-26 17:04:30 +0200

    Imported 'lazyLoadTaskManager' into the loadChildren property

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 
-import { lazyLoadPortfolioComponent, lazyLoadAboutComponent } from './lazy-load/lazyLoadComponent';
-
-import { MainComponent } from './../task-manager/main/main.component';
+import { lazyLoadPortfolioComponent, lazyLoadAboutComponent, lasyLoadTaskManager } from './lazy-load/lazyLoadComponent';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
   { path: 'portfolio', loadChildren: lazyLoadPortfolioComponent, data: { animation: 'portfolio' } },
   { path: 'about-me', loadChildren: lazyLoadAboutComponent, data: { animation: 'about-me' } },
-  { path: 'task-manager', component: MainComponent, data: { animation: 'task-manager' } }
+  { path: 'task-manager', loadChildren: lasyLoadTaskManager, data: { animation: 'task-manager' } }
 ];
