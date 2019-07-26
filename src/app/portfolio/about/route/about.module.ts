29570726a45f99b11bb2bbc21ff2f620337ee40e
Adib Zidan <94amsz@gmail.com>
2019-07-26 16:20:20 +0200

    Created the route folder

---
+++ src/app/portfolio/about/route/about.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AboutRoutingModule } from './about-routing.module';
+
+import { AboutComponent } from '../about.component';
+
+import { FooterComponent } from '../../footer/footer.component';
+
+
+@NgModule({
+  declarations: [AboutComponent, FooterComponent],
+  imports: [CommonModule, AboutRoutingModule]
+})
+
+export class AboutModule { }
