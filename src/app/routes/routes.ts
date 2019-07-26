a5086478bccf7e33b2b4c15424c48d76db91e6f6
AdibZidan <94amsz@gmail.com>
2019-07-26 09:41:48 +0200

    Implemented lazy loading feature for the 'PortfolioComponent'

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 
-import { PortfolioComponent } from '../portfolio/portfolio.component';
-
 import { AboutComponent } from './../portfolio/about/about.component';
 
-import { MainComponent } from '../task-manager/main/main.component';
+import { MainComponent } from './../task-manager/main/main.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
-  { path: 'portfolio', component: PortfolioComponent, data: { animation: 'portfolio' } },
+  {
+    path: '',
+    redirectTo: 'portfolio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'portfolio',
+    loadChildren: () => import('../portfolio/route/portfolio.module').then(module => module.PortfolioModule),
+    data: { animation: 'portfolio' }
+  },
   { path: 'about-me', component: AboutComponent, data: { animation: 'about-me' } },
-  { path: 'task-manager', component: MainComponent, data: { animation: 'task-manager' } }
+  {
+    path: 'task-manager',
+    component: MainComponent,
+    data: { animation: 'task-manager' }
+  }
 ];
