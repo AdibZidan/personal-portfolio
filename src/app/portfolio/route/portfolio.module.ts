241d93a10fd263ba8563c2f7c16edc5395c92045
AdibZidan <94amsz@gmail.com>
2019-07-26 09:40:02 +0200

    Created the 'route' folder for lazy loading

---
+++ src/app/portfolio/route/portfolio.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { PortfolioComponent } from './../portfolio.component';
+
+import { CommonModule } from '@angular/common';
+
+import { PortfolioRoutingModule } from './portfolio-routing.module';
+
+
+@NgModule({
+  declarations: [PortfolioComponent],
+  imports: [CommonModule, PortfolioRoutingModule]
+})
+
+export class PortfolioModule { }
