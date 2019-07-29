ac4691d879a3352ac275363693a827b7bdaec13f
Adib Zidan <94amsz@gmail.com>
2019-07-29 15:10:59 +0200

    Testing baseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000/tasks'
+  // baseUrl: 'http://localhost:3000/tasks'
+  baseUrl: 'http://www.adibzidan.com:3000/tasks'
 };
 
 /*
