f645ec70d192de34c0aba14c6536a8007009b260
AdibZidan <94amsz@gmail.com>
2019-07-30 08:02:21 +0200

    Exported the setLine function

--- src/assets/animations/animations.ts
+++ src/assets/animations/animations.ts
@@ -1,5 +1,7 @@
 import { trigger, transition, style, animate } from '@angular/animations';
 
+import { Task } from './../../app/task-manager/interface/Task';
+
 export const easeIn = trigger('routeAnimations', [
   transition('* <=> *', [
     style({ opacity: 0 }),
@@ -13,3 +15,9 @@ export const fadeIn = trigger('fadeIn', [
     animate('500ms', style({ opacity: 1 }))
   ])
 ]);
+
+export const setLine = (task: Task): object => {
+  const lineThrough: object = { 'is-complete': task.completed };
+
+  return lineThrough;
+};
