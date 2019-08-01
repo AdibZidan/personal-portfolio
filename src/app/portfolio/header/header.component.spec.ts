ed7ad3e8162eaad3c88a4ed3881c05a46355b5b5
AdibZidan <94amsz@gmail.com>
2019-08-01 14:41:13 +0200

    Removed extra argument

--- src/app/portfolio/header/header.component.spec.ts
+++ src/app/portfolio/header/header.component.spec.ts
@@ -1,16 +1,12 @@
 import { HeaderComponent } from './header.component';
 
-import { Title } from '@angular/platform-browser';
-
 describe('Header Component', () => {
 
   it('Should exist', () => expect(HeaderComponent).toBeDefined());
 
   it('Should be built with 1 argument; TitleService', () => {
 
-    let titleService: Title;
-
-    const headerComponent = new HeaderComponent(titleService);
+    const headerComponent = new HeaderComponent();
 
     expect(headerComponent instanceof HeaderComponent).toBeTruthy();
 
