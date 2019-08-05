e7d0e204b7eb4ce318e8c6de01a5f80adc1f240d
Zidan <adib.zidan@beko.at>
2019-08-05 17:09:34 +0200

    Added input & output tests

--- src/app/task-manager/body/body.component.spec.ts
+++ src/app/task-manager/body/body.component.spec.ts
@@ -4,6 +4,10 @@ import { TaskService } from '../services/task.service';
 
 import { Title } from '@angular/platform-browser';
 
+import { taskMock } from '../services/mock/mock';
+
+import { Task } from '../interface/Task';
+
 describe('Body Component', () => {
 
   let bodyComponent: BodyComponent, taskService: TaskService, titleService: Title;
@@ -13,5 +17,23 @@ describe('Body Component', () => {
   it('Should exist', () => expect(BodyComponent).toBeDefined());
 
   it('Should be built with 2 arguments; TaskService and Title type', () => expect(bodyComponent instanceof BodyComponent).toBeTruthy());
+
+  it(`Should have a task 'input'`, () => {
+
+    const taskInput = bodyComponent.task = taskMock;
+
+    expect(taskInput).toBe(taskMock);
+
+  });
+
+  it(`Should emit 'deleteTask' with 'onDelete' method`, () => {
+
+    const task = bodyComponent.task = taskMock;
+
+    bodyComponent.deleteTask.subscribe((taskToDelete: Task) => expect(taskToDelete).toBe(task));
+
+    bodyComponent.onDelete(task);
+
+  });
 
 });
