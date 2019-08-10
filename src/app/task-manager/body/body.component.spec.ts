eb8a76cf6d91ceed1710af13b05585f617038902
Adib Zidan <94amsz@gmail.com>
2019-08-10 20:25:37 +0200

    Updated 'BodyComponent's' Spec file and added further tests

--- src/app/task-manager/body/body.component.spec.ts
+++ src/app/task-manager/body/body.component.spec.ts
@@ -1,24 +1,60 @@
 import { BodyComponent } from './body.component';
 
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+
+import { DebugElement } from '@angular/core';
+
 import { TaskService } from '../services/task.service';
 
 import { Title } from '@angular/platform-browser';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { DialogComponent } from '../dialog/dialog.component';
+
+import { MatDialogModule, MatDialogRef } from '@angular/material';
 
 import { taskMock } from '../services/mock/mock';
 
 import { Task } from '../interface/Task';
 
-import { mockTask } from '../services/mock/mock-service.spec';
-
 describe('Body Component', () => {
 
-  let bodyComponent: BodyComponent, taskService: TaskService, titleService: Title;
+  let bodyComponent: BodyComponent,
+    bodyFixture: ComponentFixture<BodyComponent>,
+    debugElement: DebugElement,
+    htmlElement: HTMLElement,
+    taskService: TaskService,
+    titleService: Title;
 
-  beforeEach(() => bodyComponent = new BodyComponent(taskService, titleService));
+  beforeEach(async(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, MatDialogModule],
+      declarations: [BodyComponent, DialogComponent,],
+      providers: [{ provide: MatDialogRef }]
+    }).compileComponents()));
+
+  beforeEach(() => {
+
+    bodyFixture = TestBed.createComponent(BodyComponent);
+
+    bodyComponent = bodyFixture.componentInstance;
+
+    debugElement = bodyFixture.debugElement;
+
+    htmlElement = debugElement.nativeElement;
+
+    taskService = TestBed.get(TaskService);
+
+    titleService = TestBed.get(Title);
+
+  })
 
   it('Should exist', () => expect(bodyComponent).toBeDefined());
 
-  it('Should be built with 2 arguments; TaskService and Title type', () => expect(bodyComponent instanceof BodyComponent).toBeTruthy());
+  it('Should be built/compiled', () =>
+    expect(bodyComponent instanceof BodyComponent)
+      .toBeTruthy());
 
   it(`Should have a task 'input'`, () => {
 
@@ -38,15 +74,27 @@ describe('Body Component', () => {
 
   });
 
-  it('Should change the completed property of a task with the onToggleUI method', () => {
+  it(`Should toggle the completed property of a 'task' with the 'onToggleUI' method`, () => {
 
-    const task = mockTask;
+    const task = taskMock;
 
     expect(task.completed).toBeFalsy();
 
     bodyComponent.onToggleFromUI(task);
 
     expect(task.completed).toBeTruthy();
+
+  });
+
+  it(`Should change the title to 'Task Manager' after 'ngOnInit'`, () => {
+
+    bodyComponent.ngOnInit();
+
+    const expectedTitle: string = 'Task Manager';
+
+    const actualTitle: string = titleService.getTitle();
+
+    expect(expectedTitle).toBe(actualTitle);
 
   });
 
