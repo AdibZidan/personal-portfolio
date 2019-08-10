7593f1de6f5b36627e49e27d96b9eeb8d1bb35e9
Adib Zidan <94amsz@gmail.com>
2019-08-10 20:37:21 +0200

    Updated 'DialogComponent's' Spec file and added further tests

--- src/app/task-manager/dialog/dialog.component.spec.ts
+++ src/app/task-manager/dialog/dialog.component.spec.ts
@@ -1,6 +1,10 @@
 import { DialogComponent } from "./dialog.component";
 
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+
+import { DebugElement } from '@angular/core';
+
+import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material';
 
 import { FormComponent } from '../form/form.component';
 
@@ -8,13 +12,37 @@ import { taskMock } from '../services/mock/mock';
 
 describe('Dialog Component', () => {
 
-  let dialogComponent: DialogComponent, matDialog: MatDialog, matDialogRef: MatDialogRef<FormComponent>;
+  let dialogComponent: DialogComponent,
+    dialogFixture: ComponentFixture<DialogComponent>,
+    debugElement: DebugElement,
+    htmlElement: HTMLElement,
+    matDialog: MatDialog,
+    matDialogRef: MatDialogRef<FormComponent>;
 
-  beforeEach(() => dialogComponent = new DialogComponent(matDialog, matDialogRef));
+  beforeEach(async(() =>
+    TestBed.configureTestingModule({
+      imports: [MatDialogModule],
+      declarations: [DialogComponent],
+      providers: [{ provide: MatDialog }, { provide: MatDialogRef }]
+    }).compileComponents()));
+
+  beforeEach(() => {
+
+    dialogFixture = TestBed.createComponent(DialogComponent);
+
+    dialogComponent = dialogFixture.componentInstance;
+
+    debugElement = dialogFixture.debugElement;
+
+    htmlElement = debugElement.nativeElement;
+
+  });
 
   it('Should exist', () => expect(dialogComponent).toBeDefined());
 
-  it('Should be built with 2 arguments; MatDialog and MatDialogRef<FormComponent> types', () => expect(dialogComponent instanceof DialogComponent).toBeTruthy());
+  it('Should be built/compiled', () =>
+    expect(dialogComponent instanceof DialogComponent)
+      .toBeTruthy());
 
   it(`Should have a task 'input'`, () => {
 
