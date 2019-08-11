ffab468bcf825cdcd8fd3efda3ffb3637d6637f6
Adib Zidan <94amsz@gmail.com>
2019-08-11 10:03:20 +0200

    Updated 'FormComponent's' Spec file

--- src/app/task-manager/form/form.component.spec.ts
+++ src/app/task-manager/form/form.component.spec.ts
@@ -1,8 +1,12 @@
 import { FormComponent } from "./form.component";
 
-import { FormBuilder } from '@angular/forms';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 
-import { MatDialogRef } from '@angular/material';
+import { DebugElement } from '@angular/core';
+
+import { FormBuilder, FormsModule, ReactiveFormsModule, FormGroup, FormGroupDirective } from '@angular/forms';
+
+import { MatDialogRef, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material';
 
 import { Task } from '../interface/Task';
 
@@ -10,13 +14,48 @@ import { taskMock } from './../services/mock/mock';
 
 describe('Form Component', () => {
 
-  let formComponent: FormComponent, formBuilder: FormBuilder, matDialogRef: MatDialogRef<FormComponent>, data: Task;
+  let formComponent: FormComponent,
+    formFixture: ComponentFixture<FormComponent>,
+    debugElement: DebugElement,
+    htmlElement: HTMLElement,
+    formBuilder: FormBuilder,
+    matDialogRef: MatDialogRef<FormComponent>,
+    data: Task;
 
-  beforeEach(() => formComponent = new FormComponent(formBuilder, matDialogRef, data));
+
+  beforeEach(async(() =>
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatDialogModule
+      ],
+      declarations: [FormComponent],
+      providers: [
+        { provide: MatDialogRef },
+        { provide: MAT_DIALOG_DATA }
+      ]
+    }).compileComponents()));
+
+  beforeEach(() => {
+
+    formFixture = TestBed.createComponent(FormComponent);
+
+    formComponent = formFixture.componentInstance;
+
+    debugElement = formFixture.debugElement;
+
+    htmlElement = debugElement.nativeElement;
+
+    formBuilder = TestBed.get(FormBuilder);
+
+  });
 
   it('Should exist', () => expect(formComponent).toBeDefined());
 
-  it('Should be built with 3 arguments; FormBuilder, MatDialogRef<FormComponent> and Task', () => expect(formComponent instanceof FormComponent).toBeTruthy());
+  it('Should be built/compiled', () =>
+    expect(formComponent instanceof FormComponent)
+      .toBeTruthy());
 
   it(`Should have an 'undefined' task input at the start`, () => {
 
