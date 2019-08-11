1716ef74b7657a0f3e6004b75851d9c6b5a61dac
Adib Zidan <94amsz@gmail.com>
2019-08-11 15:38:19 +0200

    Updated 'FormComponent's' Spec file and added further tests

--- src/app/task-manager/form/form.component.spec.ts
+++ src/app/task-manager/form/form.component.spec.ts
@@ -4,7 +4,7 @@ import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 
 import { DebugElement } from '@angular/core';
 
-import { FormBuilder, FormsModule, ReactiveFormsModule, FormGroup, FormGroupDirective } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
 
 import { MatDialogRef, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material';
 
@@ -19,6 +19,7 @@ describe('Form Component', () => {
     debugElement: DebugElement,
     htmlElement: HTMLElement,
     formBuilder: FormBuilder,
+    formGroup: FormGroup,
     matDialogRef: MatDialogRef<FormComponent>,
     data: Task;
 
@@ -49,15 +50,23 @@ describe('Form Component', () => {
 
     formBuilder = TestBed.get(FormBuilder);
 
+    formGroup = formBuilder.group({
+      id: [''],
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(6)]],
+      percentage: [''],
+      completed: [false]
+    });
+
   });
 
-  it('Should exist', () => expect(formComponent).toBeDefined());
+  it('Should exist/be defined', () => expect(formComponent).toBeDefined());
 
   it('Should be built/compiled', () =>
     expect(formComponent instanceof FormComponent)
       .toBeTruthy());
 
-  it(`Should have an 'undefined' task input at the start`, () => {
+  it(`Should have an 'undefined' task input before 'ngOnInit'`, () => {
 
     const taskInput = formComponent.task;
 
@@ -70,6 +79,56 @@ describe('Form Component', () => {
     const taskInput = formComponent.task = taskMock;
 
     expect(taskInput).toBeDefined();
+
+  });
+
+  it(`Should have an invalid 'formGroup' if the user doesn't add the required details`, () => {
+
+    const isValid: boolean = formGroup.valid;
+
+    expect(isValid).toBeFalsy();
+
+  });
+
+  it(`Should have a valid 'formGroup' if the user adds the required details`, () => {
+
+    formGroup.controls['id'].setValue(1);
+    formGroup.controls['title'].setValue('This is the title');
+    formGroup.controls['description'].setValue('This is the description');
+    formGroup.controls['percentage'].setValue(100);
+    formGroup.controls['completed'].setValue(true);
+
+    expect(formGroup.valid).toBeTruthy();
+
+  });
+
+  it(`Should have a falsy 'isValidForm' property before 'onFormBuild' method`, () => {
+
+    const expectedIsValidForm: boolean = false;
+
+    const actualIsValidForm: boolean = formComponent.isValidForm;
+
+    expect(expectedIsValidForm).toBe(actualIsValidForm);
+
+  });
+
+  it(`Should have a 'validNumberPattern' property`, () => {
+
+    const expectedValidNumberPattern: string = '^[1-9][0-9]?$|^100$';
+
+    const actualValidNumberPattern: string = formComponent.validNumberPattern;
+
+    expect(expectedValidNumberPattern).toBe(actualValidNumberPattern);
+
+  });
+
+  it(`Should have a 'errorMessage' property`, () => {
+
+    const expectedErrorMessage: string = 'Please fill the form above';
+
+    const actualErrorMessage: string = formComponent.errorMessage;
+
+    expect(expectedErrorMessage).toBe(actualErrorMessage);
 
   });
 
