b3bc731bd3b860f6588bb7a41eaef029877e7495
Adib Zidan <94amsz@gmail.com>
2019-08-14 21:50:16 +0200

    Refactored 'AppModule'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { NgModule } from "@angular/core";
-
-import { AppComponent } from "./app.component";
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './portfolio/header/header.component';
-
-import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from "./app-routing.module";
-import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MAT_DIALOG_DATA } from '@angular/material';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
-  providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    { provide: MAT_DIALOG_DATA, useValue: {} }
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 
 export class AppModule { }
