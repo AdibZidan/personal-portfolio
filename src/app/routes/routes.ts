02ec52793879b449690a93e1870996a7976c2c84
Adib Zidan <94amsz@gmail.com>
2019-08-14 21:13:01 +0200

    Refactored 'routes'

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,10 +1,32 @@
 import { Routes } from '@angular/router';
 
-import { lazyLoadPortfolioComponent, lazyLoadAboutComponent, lazyLoadTaskManager } from './lazy-load/lazyLoadComponent';
+import {
+  lazyLoadPortfolioComponent,
+  lazyLoadAboutComponent,
+  lazyLoadTaskManager
+} from './lazy-load/lazyLoadComponent';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
-  { path: 'portfolio', loadChildren: lazyLoadPortfolioComponent, data: { animation: 'portfolio' } },
-  { path: 'about-me', loadChildren: lazyLoadAboutComponent, data: { animation: 'about-me' } },
-  { path: 'task-manager', loadChildren: lazyLoadTaskManager, data: { animation: 'task-manager' } }
+  {
+    path: '',
+    redirectTo: 'portfolio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'portfolio',
+    loadChildren: lazyLoadPortfolioComponent,
+    data: { animation: 'portfolio' }
+  },
+  {
+    path: 'about-me',
+    loadChildren: lazyLoadAboutComponent,
+    data: { animation: 'about-me' }
+  },
+  {
+    path: 'task-manager',
+    loadChildren: lazyLoadTaskManager,
+    data: {
+      animation: 'task-manager'
+    }
+  }
 ];
