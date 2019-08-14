b8ef32fc78852093ee76902a2a21a5c78c741a17
Adib Zidan <94amsz@gmail.com>
2019-08-14 21:07:40 +0200

    Refactored 'HeaderComponent'

--- src/app/portfolio/header/header.component.spec.ts
+++ src/app/portfolio/header/header.component.spec.ts
@@ -1,37 +1,37 @@
 import { HeaderComponent } from './header.component';
-
 import { ComponentFixture, async, TestBed } from '@angular/core/testing';
-
 import { DebugElement } from '@angular/core';
-
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('Header Component', () => {
 
-  let headerComponent: HeaderComponent,
-    headerFixture: ComponentFixture<HeaderComponent>,
-    debugElement: DebugElement,
-    htmlElement: HTMLElement;
+  let headerComponent: HeaderComponent;
+  let headerFixture: ComponentFixture<HeaderComponent>;
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [HeaderComponent]
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        HeaderComponent
+      ]
     }).compileComponents()));
 
   beforeEach(() => {
 
     headerFixture = TestBed.createComponent(HeaderComponent);
-
     headerComponent = headerFixture.componentInstance;
-
     debugElement = headerFixture.debugElement;
-
     htmlElement = debugElement.nativeElement;
 
   });
 
-  it('Should exist/be defined', () => expect(headerComponent).toBeDefined());
+  it('Should exist/be defined', () =>
+    expect(headerComponent)
+      .toBeDefined());
 
   it('Should be built/compiled', () =>
     expect(headerComponent instanceof HeaderComponent)
