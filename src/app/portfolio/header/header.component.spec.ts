3e1ecaeda281e5472921d539af3f54ffec7012cd
Adib <94amsz@gmail.com>
2019-08-16 11:47:09 +0200

    Refactored 'HeaderComponent'

--- src/app/portfolio/header/header.component.spec.ts
+++ src/app/portfolio/header/header.component.spec.ts
@@ -7,26 +7,22 @@ describe('Header Component', () => {
 
   let headerComponent: HeaderComponent;
   let headerFixture: ComponentFixture<HeaderComponent>;
+
   let debugElement: DebugElement;
   let htmlElement: HTMLElement;
 
   beforeEach(async(() =>
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        HeaderComponent
-      ]
+      imports: [RouterTestingModule],
+      declarations: [HeaderComponent]
     }).compileComponents()));
 
   beforeEach(() => {
-
     headerFixture = TestBed.createComponent(HeaderComponent);
     headerComponent = headerFixture.componentInstance;
+
     debugElement = headerFixture.debugElement;
     htmlElement = debugElement.nativeElement;
-
   });
 
   it('Should exist/be defined', () =>
@@ -38,39 +34,29 @@ describe('Header Component', () => {
       .toBeTruthy());
 
   it(`Should have a 'header' tag with the 'header' class`, () => {
-
     const header: Element = htmlElement.querySelector('header.header');
 
     expect(header).toBeTruthy();
-
   });
 
   it(`Should have a 'nav' tag with the 'menu' class`, () => {
-
     const nav: Element = htmlElement.querySelector('nav.menu');
 
     expect(nav).toBeTruthy();
-
   });
 
   it(`Should have an 'ul' tag with the 'menu-navigation' class`, () => {
-
     const ul: Element = htmlElement.querySelector('ul.menu-navigation');
 
     expect(ul).toBeTruthy();
-
   });
 
   it(`Should have 3 'li' & 'a' tags with the 'navigation-item' & 'navigation-link' classes respectively`, () => {
-
     const totalAmountOfLiTags: number = htmlElement.querySelectorAll('li.navigation-item').length;
-
-    expect(totalAmountOfLiTags).toBe(3);
-
     const totalAmountOfATags: number = htmlElement.querySelectorAll('a.navigation-link').length;
 
+    expect(totalAmountOfLiTags).toBe(3);
     expect(totalAmountOfATags).toBe(3);
-
   });
 
 });
