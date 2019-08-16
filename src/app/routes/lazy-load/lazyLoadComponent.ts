19320906cdcd50ad28734355cfbeef218216e50c
Adib <94amsz@gmail.com>
2019-08-16 11:49:32 +0200

    Refactored 'lazyLoadComponent'

--- src/app/routes/lazy-load/lazyLoadComponent.ts
+++ src/app/routes/lazy-load/lazyLoadComponent.ts
@@ -1,28 +1,22 @@
 import { LoadChildrenCallback } from '@angular/router';
 
 export const lazyLoadPortfolioComponent: LoadChildrenCallback = async () => {
-
     const module = await import('../../portfolio/route/portfolio.module');
     const portfolioModule = module.PortfolioModule;
 
     return portfolioModule;
-
 };
 
 export const lazyLoadAboutComponent: LoadChildrenCallback = async () => {
-
     const module = await import('../../portfolio/about/route/about.module');
     const aboutModule = module.AboutModule;
 
     return aboutModule;
-
 };
 
 export const lazyLoadTaskManager: LoadChildrenCallback = async () => {
-
     const module = await import('../../task-manager/route/task-manager.module');
     const taskManagerModule = module.TaskManagerModule;
 
     return taskManagerModule;
-
 };
