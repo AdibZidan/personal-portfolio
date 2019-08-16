399eac20ef0574665a03d7de508b665a163c7ec6
Adib <94amsz@gmail.com>
2019-08-16 12:07:57 +0200

    Refactored 'TaskServiceSpec'

--- src/app/task-manager/services/task.service.spec.ts
+++ src/app/task-manager/services/task.service.spec.ts
@@ -8,28 +8,24 @@ describe('Task Service', () => {
 
     let taskService: TaskService;
     let httpTestingController: HttpTestingController;
+
     let baseUrl: string;
     let tasks: Task[];
     let task: Task;
 
     beforeEach(async(() =>
         TestBed.configureTestingModule({
-            imports: [
-                HttpClientTestingModule
-            ],
-            providers: [
-                TaskService
-            ]
+            imports: [HttpClientTestingModule],
+            providers: [TaskService]
         })));
 
     beforeEach(() => {
-
         taskService = TestBed.get(TaskService);
         httpTestingController = TestBed.get(HttpTestingController);
+
         baseUrl = taskService.baseUrl;
         tasks = tasksMock;
         task = taskMock;
-
     });
 
     afterEach(() => httpTestingController.verify());
@@ -43,120 +39,95 @@ describe('Task Service', () => {
             .toBeTruthy());
 
     it(`Should have a 'baseUrl'`, () => {
-
         const expectedBaseUrl = 'https://localhost:8000/tasks';
         const actualBaseUrl: string = taskService.baseUrl;
 
         expect(expectedBaseUrl).toBe(actualBaseUrl);
-
     });
 
     it(`Should get the 'refresher' method`, () => {
-
         const refresher = taskService.refresher;
 
         expect(refresher).toBeTruthy();
-
     });
 
     it('Should retrieve tasks via a GET request', () => {
-
         taskService
             .getTasksFromBackEnd()
             .subscribe((tasksToGet: Task[]) => {
-
                 const expectedLength = 3;
 
                 const actualLength: number = tasksToGet.length;
 
                 expect(expectedLength).toBe(actualLength);
-
                 expect(tasksToGet).toEqual(tasks);
-
             });
 
         const request: TestRequest = httpTestingController.expectOne(baseUrl);
-
         const method: string = request.request.method;
 
         expect(method).toBe('GET');
 
         request.flush(tasks);
-
     });
 
     it('Should add tasks via a POST request', () => {
-
         taskService
             .addTaskToBackEnd(task)
             .subscribe((taskToAdd: Task) =>
                 expect(taskToAdd).toBe(task));
 
         const request: TestRequest = httpTestingController.expectOne(baseUrl);
-
         const method: string = request.request.method;
 
         expect(method).toBe('POST');
 
         request.flush(task);
-
     });
 
     it('Should toggle a task via a PUT request', () => {
-
         taskService
             .toggleTaskFromBackEnd(task)
             .subscribe((taskToToggle: Task) =>
                 expect(taskToToggle).toBe(task));
 
         const baseUrlPutRequest = `${baseUrl}/update/${task.id}`;
-
         const request: TestRequest = httpTestingController.expectOne(baseUrlPutRequest);
-
         const method: string = request.request.method;
 
         expect(method).toBe('PUT');
 
         request.flush(task);
-
     });
 
     it('Should edit a task via a PUT request', () => {
-
         taskService
             .editTaskFromBackEnd(task)
             .subscribe((taskToEdit: Task) =>
                 expect(taskToEdit).toBe(task));
 
         const baseUrlPutRequest = `${baseUrl}/edit/${task.id}`;
-
         const request: TestRequest = httpTestingController.expectOne(baseUrlPutRequest);
-
         const method: string = request.request.method;
 
         expect(method).toBe('PUT');
 
         request.flush(task);
-
     });
 
     it('Should delete a task via a DELETE request', () => {
-
         taskService
             .deleteTaskFromBackEnd(task)
             .subscribe((taskToDelete: Task) =>
                 expect(taskToDelete).toBe(task));
 
         const baseUrlDeleteRequest = `${baseUrl}/delete/${task.id}`;
-
         const request: TestRequest = httpTestingController.expectOne(baseUrlDeleteRequest);
-
         const method: string = request.request.method;
 
         expect(method).toBe('DELETE');
 
         request.flush(task);
-
     });
 
 });
