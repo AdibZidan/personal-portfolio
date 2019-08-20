858c7ade94a4de8009bca6a07ef61a3c716cf19e
Adib <94amsz@gmail.com>
2019-08-20 15:09:36 +0200

    Applied 'Insert 2 Spaces' rule to 'LazyLoadComponent'

--- src/app/routes/lazy-load/lazyLoadComponent.ts
+++ src/app/routes/lazy-load/lazyLoadComponent.ts
@@ -1,22 +1,22 @@
 import { LoadChildrenCallback } from '@angular/router';
 
 export const lazyLoadPortfolioComponent: LoadChildrenCallback = async () => {
-    const module = await import('../../portfolio/route/portfolio.module');
-    const portfolioModule = module.PortfolioModule;
+  const module = await import('../../portfolio/route/portfolio.module');
+  const portfolioModule = module.PortfolioModule;
 
-    return portfolioModule;
+  return portfolioModule;
 };
 
 export const lazyLoadAboutComponent: LoadChildrenCallback = async () => {
-    const module = await import('../../portfolio/about/route/about.module');
-    const aboutModule = module.AboutModule;
+  const module = await import('../../portfolio/about/route/about.module');
+  const aboutModule = module.AboutModule;
 
-    return aboutModule;
+  return aboutModule;
 };
 
 export const lazyLoadTaskManager: LoadChildrenCallback = async () => {
-    const module = await import('../../task-manager/route/task-manager.module');
-    const taskManagerModule = module.TaskManagerModule;
+  const module = await import('../../task-manager/route/task-manager.module');
+  const taskManagerModule = module.TaskManagerModule;
 
-    return taskManagerModule;
+  return taskManagerModule;
 };
