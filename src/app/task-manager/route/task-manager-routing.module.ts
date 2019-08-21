413dcaf4de2a533401551a90899aed1ac92ed413
Adib <94amsz@gmail.com>
2019-08-21 13:54:21 +0200

    Refactored 'TaskManagerRoutingModule'

--- src/app/task-manager/route/task-manager-routing.module.ts
+++ src/app/task-manager/route/task-manager-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from '../main/main.component';
 
-const routes: Routes = [{ path: '', component: MainComponent }];
+const routes: Routes = [
+  { path: '', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
