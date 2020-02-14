62cd2d0b13636dc9cca141f2cbf0a1c76a65bb8e
Adib Zidan <94amsz@gmail.com>
2020-02-14 07:01:47 +0100

    Use fadeIn animation in HeaderComponent

--- src/app/portfolio/header/header.component.ts
+++ src/app/portfolio/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeIn } from 'src/assets/animations/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [fadeIn]
 })
 
 export class HeaderComponent implements OnInit {
