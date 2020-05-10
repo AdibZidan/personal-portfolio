7bacbf34fde52a3476e1cfbeae246770eebbe78e
Adib Zidan <94amsz@gmail.com>
2020-05-10 20:17:42 +0200

    Fix GoogleAnalyticsService test

--- src/app/google-analytics/google-analytics.service.spec.ts
+++ src/app/google-analytics/google-analytics.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { GoogleAnalyticsService } from './google-analytics.service';
 
 describe('Google Analytics Service', () => {
@@ -8,7 +9,10 @@ describe('Google Analytics Service', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
     });
   }));
 
